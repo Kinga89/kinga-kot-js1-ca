7b252df5f6ab9d0f51a765ebf69a8f4f64c19a5c
Kinga89 <kinga-kot@hotmail.com>
2022-05-03 22:03:48 +0200

    added header, general page styling + js

--- js/details.js
+++ js/details.js
@@ -12,34 +12,43 @@ if (id === null) {
     location.href = "/";
 }
 
-const detailsURL = "http://makeup-api.herokuapp.com/api/v1/products.json?product_tags=vegan" + id;
+const detailsURL =   'http://makeup-api.herokuapp.com/api/v1/products/' + id + '.json';
+
+console.log (detailsURL)
 
 async function fetchMakeupDetails() {
     try {
         const details = await fetch(detailsURL);
         const result = await details.json();
         console.log(result);
-
+        
         makeupDetails.innerHTML = "";
 
-        makeupDetails.innerHTML = `<h2>${result.name}</h2>
+
+       
+
+        makeupDetails.innerHTML = 
+        ` <div class="detail-description">
+        <h3>${result.name}</h3>
         <p>Brand: ${result.brand}</p>
-        <p>Product details: ${result.description}</p>
-        <img src="${result.image_link}" alt"${result.name}"></img>
+        <p>Brand: ${result.price}$ ${result.currency}</p>
+        <a href="${result.product_link}"><p>Link to the product</p> </a>
+        <p>Product details: ${result.description}</p></div>
+        <img src="${result.image_link}" alt="${result.name}" class="detail-img"></img>
         `;
+
+       
+
+      
 
     }
     catch (error) {
 
         makeupDetails.innerHTML = `<div id="error_msg">An error occured when calling the API </div>`;
-
         console.log(error);
     }
-
-
 
 }
 
 fetchMakeupDetails();
 
-console.log (fetchMakeupDetails())
